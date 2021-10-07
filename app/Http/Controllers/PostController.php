6b18cbce1a449acb53e8d22d0a4dc6eadda5db38
K-tsuba <t.ko-he-1999.622.fifteen@docomo.ne.jp>
2021-10-07 18:10:30 +0900

    [add]記事詳細画面

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use  Illuminate\Http\Request;
+use  App\Post;
+
+
+class PostController extends Controller
+{
+    public function index(Post $post)
+    {
+        // return view('posts/index')->with(['posts' => $post->get()]);  
+        
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+}
