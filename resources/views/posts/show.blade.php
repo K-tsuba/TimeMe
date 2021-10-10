7c1f976ba1f02651f5319fcba8049befaa21f949
K-tsuba <t.ko-he-1999.622.fifteen@docomo.ne.jp>
2021-10-10 12:07:17 +0900

    ブログ投稿の削除

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,11 +15,24 @@
     <body>
         <h1>Blog Name</h1>
         <p class="edit">[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post" style="display:inline">
+            @csrf
+            @method('DELETE')
+            <button type="submit">[<span onclick="return deletePost(this);">delete</span>]</button>
+        </form>
         <div class='post'>
             <h2 class='title'>{{ $post->title }}</h2>
             <p class='body'>{{ $post->body }}</p>
             <p class='updated_at'>{{ $post->updated_at }}</p>
         </div>
        <div class="back">{<a href="/">back</a>}</div>
+       <script>
+           function deletePost(e){
+               'use strict';
+               if (confirm('復元することはできません。\n本当に削除しますか？')){
+                   document.getElementbyld('form_delete').submit();
+               }
+           }
+       </script>
     </body>
 </html>
