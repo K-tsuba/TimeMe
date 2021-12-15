4637cd3c1d68586fe05a393da406ea3f5bb06eb9
K-tsuba <t.ko-he-1999.622.fifteen@docomo.ne.jp>
2021-12-15 22:49:42 +0900

    Twitter Apiの設定変更

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -20,10 +20,10 @@ class TweetController extends Controller
         $tweet->save();
         if ($status == 1) { //$statusは投稿の公開ステータス 0：保留、1：公開
             $twitter = new TwitterOAuth(
-                env('TWITTER_CONSUMER_KEY'),
-                env('TWITTER_CONSUMER_SECRET'),
-                env('TWITTER_ACCESS_TOKEN'),
-                env('TWITTER_ACCESS_SECRET')
+                config('services.twitter.consumer_key'),
+                config('services.twitter.consumer_secret'),
+                config('services.twitter.access_token'),
+                config('services.twitter.access_secret')
             );
             $twitter->post("statuses/update", [
                 "status" => $tweet->goal 
@@ -39,10 +39,10 @@ class TweetController extends Controller
         $tweet->save();
         if ($status == 1) { //$statusは投稿の公開ステータス 0：保留、1：公開
             $twitter = new TwitterOAuth(
-                env('TWITTER_CONSUMER_KEY'),
-                env('TWITTER_CONSUMER_SECRET'),
-                env('TWITTER_ACCESS_TOKEN'),
-                env('TWITTER_ACCESS_SECRET')
+                config('services.twitter.consumer_key'),
+                config('services.twitter.consumer_secret'),
+                config('services.twitter.access_token'),
+                config('services.twitter.access_secret')
             );
             $twitter->post("statuses/update", [
                 "status" => $tweet->review 
